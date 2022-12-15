5ffc46d4a2a58be2c97d1bebb289c92a32cfa8f1
alizadeh6100 <119208089+alizadeh6100@users.noreply.github.com>
2022-12-15 02:24:09 -0800

    Add files via upload

---
+++ pizaa1.py
@@ -0,0 +1,12 @@
+x = 0
+size = input("small / large / medium:")
+if size == "small":
+    x += 15
+elif size == "medium":
+    x += 20
+elif size == "large":
+    x += 25
+else:
+    print("error")
+print(x)
+sp = input("small_peperoni / medium_large_peperoni")
